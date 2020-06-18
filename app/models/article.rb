6b3de42c87b8d9a6e34ade42f8f1a36e88b356f2
Emilio <galindo.hayashi@gmail.com>
2020-06-18 01:35:11 -0500

    se agrego columna de password para usuarios

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article <ApplicationRecord
-
+  belongs_to :user
+  
   validates :tittle, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 
-  belongs_to :user
+
 end
