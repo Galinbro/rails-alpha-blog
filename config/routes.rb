11ac4f55c5322f007e2aad0b96280fe67f4d99cf
Emilio <galindo.hayashi@gmail.com>
2020-06-18 14:19:46 -0500

    create sessions and auths

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#signup'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 end
