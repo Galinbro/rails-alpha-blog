d57a827162cb136aa4e52589d58d355212679272
Emilio <galindo.hayashi@gmail.com>
2020-06-18 12:46:06 -0500

    se agrego show page a usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+  
   def signup
     @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
-
   end
   
   def update
