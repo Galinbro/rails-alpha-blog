de02f4f107da5e4b62c61517ec704d03530f7d55
Emilio <galindo.hayashi@gmail.com>
2020-06-18 12:10:41 -0500

    Se crea e usuario en el sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the alpha blog #{@user.username}, you have successfully signup."
+      redirect_to articles_path
+    else
+      render 'signup'
+    end
   end
   
+  private 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end 
