8595006d079a49c1027451c570759fd406908a0a
Emilio <galindo.hayashi@gmail.com>
2020-06-18 13:11:30 -0500

    fix - rutas al crear usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @articles = @user.articles
   end
+
+  def index
+    @users = User.all
+  end
+  
   
   def signup
     @user = User.new
@@ -17,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully edited."
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
