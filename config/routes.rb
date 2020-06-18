01fe6d281b65077f61722caa7410cca272d70b05
Emilio <galindo.hayashi@gmail.com>
2020-06-18 18:53:48 -0500

    TTD - categories controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
 
+          # Home #
   root 'pages#home'
+          # About #
   get 'about', to:'pages#about'
+          # Articles #
   resources :articles
+          # Sign up #
   get 'signup', to: 'users#signup'
   resources :users, except: [:new]
-
+          # Log in #
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'login', to: 'sessions#destroy'
+            # Categories #
+  resources :categories, except: [:destroy]
+
 end
