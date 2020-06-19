3aa0a41cfb09f3fe62f2b8b54dffd62b43c3fb4a
Emilio <galindo.hayashi@gmail.com>
2020-06-18 22:15:09 -0500

    TDD integracion - validar que no se cree la categoria en caso de error

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test "get new category form and creating the catgory" do
+  
+  test "get new category form and creating the category" do
     get '/categories/new'
     assert_response :success
     assert_difference 'Category.count', 1 do
@@ -11,5 +12,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid category submision" do
+    get '/categories/new'
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: ""} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
   end
 end
